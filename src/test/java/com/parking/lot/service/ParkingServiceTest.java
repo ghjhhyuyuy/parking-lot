e59d575606d8eaba29decbda42ce50dd9bce7a3e
PengShuai <speng@thoughtworks.com>
2021-01-19 14:04:30 +0800

    [Zuowen Wang]Add test should_throw_NotFoundException_when_not_found_park and should_throw_OverSizeException_when_over_park_size.

--- src/test/java/com/parking/lot/service/ParkingServiceTest.java
+++ src/test/java/com/parking/lot/service/ParkingServiceTest.java
@@ -2,11 +2,14 @@ package com.parking.lot.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 
 import com.parking.lot.entity.Parking;
 import com.parking.lot.entity.Ticket;
+import com.parking.lot.exception.OverSizeException;
 import com.parking.lot.repository.ParkingRepository;
 import com.parking.lot.repository.TicketRepository;
 import java.util.Optional;
@@ -34,6 +37,31 @@ class ParkingServiceTest {
     Ticket returnTicket = parkingService.parkingCar(parking.getId());
     assertNotNull(returnTicket);
     assertEquals(parkingEmpty,parking.getSize());
+  }
+
+  @Test
+  void should_throw_NotFoundException_when_not_found_park() {
+    when(parkingRepository.findById(anyString())).thenReturn(Optional.empty());
+    assertThrows(
+        NotFoundException.class,
+        () -> {
+          parkingService.parkingCar("123");
+        },"not found parking");
+  }
+
+  @Test
+  void should_throw_OverSizeException_when_over_park_size() {
+    Parking parking = getFullParking();
+    when(parkingRepository.findById(parking.getId())).thenReturn(Optional.of(parking));
+    assertThrows(
+        OverSizeException.class,
+        () -> {
+          parkingService.parkingCar(parking.getId());
+        });
+  }
+
+  private Parking getFullParking() {
+    return Parking.builder().id("42f408b2-3ee6-48fd-8159-b49789f7096c").size(0).build();
   }
 
   private Parking getParking() {
