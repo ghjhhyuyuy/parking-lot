d394287a4ff8098f426957a9e3342bf5f7c8062c
PengShuai <speng@thoughtworks.com>
2021-01-19 13:36:19 +0800

    [Zuowen Wang]Add origin data.

---
+++ src/main/resources/db/migration/V20210119133233__add_park_in_table_parking.sql
@@ -0,0 +1,4 @@
+INSERT INTO "parking" ("id", "size")
+VALUES ('42f408b2-3ee6-48fd-8159-b49789f7096b', 200),
+       ('a0be2b8a-b18b-4e16-843c-8175120b0dbc', 50),
+       ('b1e1946e-101c-403a-92fa-859081905dcf', 20);
