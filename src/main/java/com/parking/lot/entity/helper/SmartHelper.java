4f81bc97df099565efccab1dc52898ceef04f939
PengShuai <speng@thoughtworks.com>
2021-01-21 15:34:48 +0800

    [Zuowen Wang]Pass test should_get_parking_when_given_smart_helper.

--- src/main/java/com/parking/lot/entity/helper/SmartHelper.java
+++ src/main/java/com/parking/lot/entity/helper/SmartHelper.java
@@ -1,12 +1,18 @@
 package com.parking.lot.entity.helper;
 
 import com.parking.lot.entity.Parking;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class SmartHelper extends ParkingHelper{
   @Override
   public void parking(List<Parking> parkings) {
-
+    Optional<Parking> optionalParking = parkings.stream().max(Comparator.comparingInt(Parking::getSize));
+    if(optionalParking.isPresent()){
+      Parking parking = optionalParking.get();
+      parking.reduceSize();
+    }
   }
 
 }
