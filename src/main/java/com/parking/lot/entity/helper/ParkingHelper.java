84b272d7370e9a88ebeae3ef6b406aa5dd43e482
PengShuai <speng@thoughtworks.com>
2021-02-02 11:06:28 +0800

    [Zuowen Wang]Refactor.

--- src/main/java/com/parking/lot/entity/helper/ParkingHelper.java
+++ src/main/java/com/parking/lot/entity/helper/ParkingHelper.java
@@ -2,11 +2,9 @@ package com.parking.lot.entity.helper;
 
 import com.parking.lot.entity.Parking;
 import java.util.List;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public abstract class ParkingHelper {
 
   final boolean isAllow = true;
