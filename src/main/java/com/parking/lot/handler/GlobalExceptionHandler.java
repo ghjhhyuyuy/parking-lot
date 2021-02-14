f6db2d0219de8f117ad3580722a74d12c9fe0fed
zuowen.wang <zuowen.wang@rea-group.com>
2021-02-14 21:27:36 +0800

    [Zuowen Wang]Fix exception handler.

--- src/main/java/com/parking/lot/handler/GlobalExceptionHandler.java
+++ src/main/java/com/parking/lot/handler/GlobalExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.parking.lot.handler;
 
 import com.parking.lot.entity.vo.Result;
+import com.parking.lot.exception.IllegalSizeException;
 import com.parking.lot.exception.IllegalTicketException;
 import com.parking.lot.exception.MyException;
 import com.parking.lot.exception.NoMatchingRoleException;
 import com.parking.lot.exception.NotFoundResourceException;
 import com.parking.lot.exception.NotManagerUserException;
 import com.parking.lot.exception.NotParkingHelperException;
+import com.parking.lot.exception.NotRightCarException;
 import com.parking.lot.exception.OutOfSetException;
 import com.parking.lot.exception.OverSizeException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,10 +17,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-  
+
   @ExceptionHandler({OverSizeException.class, IllegalTicketException.class,
-      NotFoundResourceException.class, NotManagerUserException.class,
-      NotParkingHelperException.class, OutOfSetException.class, NoMatchingRoleException.class})
+      NotFoundResourceException.class, NotManagerUserException.class, IllegalSizeException.class,
+      NotRightCarException.class, NotParkingHelperException.class, OutOfSetException.class,
+      NoMatchingRoleException.class})
   @ResponseBody
   public Result<Object> overSizeExceptionHandler(MyException e) {
     return Result.getExceptionResult(e.getExceptionMessage().getCode(),
