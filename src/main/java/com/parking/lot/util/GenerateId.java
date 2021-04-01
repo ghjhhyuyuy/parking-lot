9e9ab090a723647ba57f7026772cf42422d21187
zuowen.wang <zuowen.wang@rea-group.com>
2021-04-01 09:45:48 +0800

    [Zuowen Wang]Add lock in GenerateId.

--- src/main/java/com/parking/lot/util/GenerateId.java
+++ src/main/java/com/parking/lot/util/GenerateId.java
@@ -1,10 +1,15 @@
 package com.parking.lot.util;
 
 import java.util.UUID;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class GenerateId {
-//todo lock
-  public static String getUUID() {
-    return UUID.randomUUID().toString();
-  }
+    private static final ReentrantLock reentrantLock = new ReentrantLock();
+
+    public static String getUUID() {
+        reentrantLock.lock();
+        String id = UUID.randomUUID().toString();
+        reentrantLock.unlock();
+        return id;
+    }
 }
